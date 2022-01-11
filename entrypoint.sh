76711cb54f83146e072075a82a2541585f859d8d
Christer Edvartsen <christer.edvartsen@nav.no>
2022-01-11 13:37:32 +0100

    add more debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,9 +45,13 @@ echo "prepare keys"
 mkdir -p $TMP_HOME/.ssh
 chmod 0700 $TMP_HOME/.ssh
 
+echo "create private and public keys"
+
 echo "$SSH_PRIVATE_KEY" > $TMP_HOME/.ssh/id
 echo "$SSH_PUBLIC_KEY" > $TMP_HOME/.ssh/id.pub
 chmod 600 $TMP_HOME/.ssh/id $TMP_HOME/.ssh/id.pub
+
+echo "gather SSH public keys"
 
 ssh-keyscan github.com > $TMP_HOME/.ssh/known_hosts
 # ssh-keyscan "$REMOTE_HOST" >> $TMP_HOME/.ssh/known_hosts
