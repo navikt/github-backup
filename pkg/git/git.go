5be6639016c6ebc1112ac618ec8616c8cae0950a
J-K. Solbakken <jksolbakken@gmail.com>
2022-11-14 10:58:15 +0100

    rudimentary functionality for cloning repos and compressing them

---
+++ pkg/git/git.go
@@ -0,0 +1,26 @@
+package git
+
+import (
+	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func CloneRepo(basedir, repo, username, password string) error {
+	p := filepath.Join(basedir, repo)
+	if strings.ContainsAny(p, "..") {
+		return fmt.Errorf("%s seems like a bogus directory", p)
+	}
+	_, err := git.PlainClone(p, false, &git.CloneOptions{
+		URL:      fmt.Sprintf("https://github.com/%s", repo),
+		Progress: os.Stdout,
+		Auth: &http.BasicAuth{
+			Username: username,
+			Password: password,
+		},
+	})
+	return err
+}
---
+++ pkg/zippings/compress.go
@@ -0,0 +1,55 @@
+package zippings
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func CompressIt(src, compressedFilename string) error {
+	tarGzFile, err := os.OpenFile(compressedFilename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600)
+	defer tarGzFile.Close()
+	if err != nil {
+		return err
+	}
+	gzw := gzip.NewWriter(tarGzFile)
+	defer gzw.Close()
+	tw := tar.NewWriter(gzw)
+	defer tw.Close()
+
+	return filepath.Walk(src, func(file string, fileInfo os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !fileInfo.Mode().IsRegular() {
+			return nil
+		}
+		// do not re-compress our own file
+		if strings.HasSuffix(compressedFilename, fileInfo.Name()) {
+			return nil
+		}
+		header, err := tar.FileInfoHeader(fileInfo, fileInfo.Name())
+		if err != nil {
+			return err
+		}
+		header.Name = strings.TrimPrefix(strings.Replace(file, src, "", -1), string(filepath.Separator))
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		if _, err := io.Copy(tw, f); err != nil {
+			return err
+		}
+		if err := f.Close(); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
